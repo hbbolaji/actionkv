61540be43226c41870cc988e2a044ad499309cdd
Hbbolaji <bolaji1050@gmail.com>
2026-09-10 14:06:10 +0100

    feat: peforming CRUD ops on actionkv with skeleton methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
+use std::{io, path::Path};
 
+pub struct ActionKV {}
+
+impl ActionKV {
+    pub fn open(fname: &Path) -> io::Result<ActionKV> {
+        Ok(ActionKV {})
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+
+    pub fn get(&self, key: &str) -> io::Result<Option<()>> {
+        Ok(Some(()))
+    }
+
+    pub fn delete(&self, key: &str) -> io::Result<()> {
+        Ok(())
+    }
+
+    pub fn insert(&self, key: &str, value: &str) -> io::Result<()> {
+        Ok(())
+    }
+
+    pub fn update(&self, key: &str, value: &str) -> io::Result<()> {
+        Ok(())
+    }
+}
